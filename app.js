f568a0e39889e323b980440e4e65b5ea901deb75
jones-chen <tonnyqoo@gmail.com>
2023-12-20 13:57:15 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 // Include express from node_modules and define server related variables
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
 const app = express()
 const port = 3000
+
+//  僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// check DB 取得資料庫連線狀態
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting the route and corresponding response
 app.get('/', (req, res) => {

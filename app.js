8c95c65ca7e0c71a6eca41954d439d3337af7dfe
jones-chen <tonnyqoo@gmail.com>
2023-12-21 13:57:19 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-// Include express from node_modules and define server related variables
+// 一、導入模組Include express from node_modules and define server related variables
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
+
+const mongoose = require('mongoose') // 載入 mongoose
+const Todo = require('./models/todo') // 載入 Todo model
 
 const app = express()
 const port = 3000
@@ -11,7 +13,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-// 資料庫設定 - check DB 取得資料庫連線狀態
+// 二、資料庫設定 - check DB 取得資料庫連線狀態
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 const db = mongoose.connection
 // 連線異常
@@ -23,13 +25,16 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 設定模板引擎
+// 三、視覺模板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main' , extname : '.hbs'}))  //讀取main.handlebars作為樣板佈局/主要佈局
 app.set('view engine', 'hbs') //根據這裡的設定當作副檔名
 
-// 設定路由
+// 四、設定路由
 app.get('/', (req, res) => {
-    res.render('index')   //原本是send，改成render 佈局＋index的局部佈局
+  Todo.find() // 取出 Todo model 裡的所有資料，也可以傳入參數
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos:todos })) // 取得陣列叫todos，將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 // Listen the server when it started
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,6 +1,7 @@
 //種子資料產生器
 const mongoose = require('mongoose')
-const Todo = require('../todo') // 載入 todo model
+const Todo = require('../todo') // 載入 todo model, 沒有給副檔名的時候，Node會自動幫你加上.js , .json,  .node
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }

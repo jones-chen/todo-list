7f13a48c63dc74d62fe53b1ff30873a5a87047c6
jones-chen <tonnyqoo@gmail.com>
2023-12-25 14:02:29 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 // 一、導入模組Include express from node_modules and define server related variables
 const express = require('express')
 const exphbs = require('express-handlebars')
-
+const methodOverride = require('method-override') 
 const mongoose = require('mongoose') // 載入 mongoose
 const Todo = require('./models/todo') // 載入 Todo model
 
 const app = express()
 const port = 3000
 
-//  僅在非正式環境時, 使用 dotenv
+
+// 二、前置處理
+// 1 -  USE
+// (1-1) 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-//Bady-parser
+// (1-2) Bady-parser
 app.use(express.urlencoded({ extended: true }))
 
-// 二、資料庫設定 - check DB 取得資料庫連線狀態
+// (1-3) post 改變成 delete, put
+app.use(methodOverride('_method')) // 設定每一筆請求都會透過 methodOverride 進行前置處理
+
+// 2 -  check DB 取得資料庫連線狀態
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 const db = mongoose.connection
-// 連線異常
+// (2-1) 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
+// (2-2)連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 三、視覺模板引擎
+// 3 - 視覺模板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main' , extname : '.hbs'}))  //讀取main.handlebars作為樣板佈局/主要佈局
 app.set('view engine', 'hbs') //根據這裡的設定當作副檔名
 
@@ -74,7 +80,7 @@ app.get('/todos/:id/edit', (req, res) => {
 })
 
 // 編輯：將edit頁面的參數丟入資料庫存檔
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body  //取出更新後的資料，使用“解構賦值”
   return Todo.findById(id)
@@ -88,7 +94,7 @@ app.post('/todos/:id/edit', (req, res) => {
 })
 
 // 刪除：將edit頁面的參數丟入資料庫存檔
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .then(todo => {return todo.remove()})  //
